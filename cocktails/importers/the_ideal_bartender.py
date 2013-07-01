ec0d58a192dee37364d62730e12e2070ea7a4295
Matthew Hooker <mwhooker@gmail.com>
2013-07-01 02:46:06 -0700

    parsing beautifully.

--- cocktails/importers/the_ideal_bartender.py
+++ cocktails/importers/the_ideal_bartender.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 import paths
 import os.path
+from itertools import imap, ifilter
 
 
 tib = os.path.join( paths.SOURCES_PATH, 'other/THE IDEAL BARTENDER')
@@ -12,7 +13,8 @@ Section = namedtuple('Section', ('title', 'body'))
 
 thebook = []
 
-title = body = None
+title = None
+body = []
 
 def filter_book(f):
     started = False
@@ -25,11 +27,11 @@ def filter_book(f):
             if line != 'DEDICATED':
                 continue
             else:
-                print "starting parse"
+                # print "starting parse"
                 started = True
 
         if line.startswith("***END OF THE PROJECT GUTENBERG EBOOK"):
-            print "ending parse"
+            # print "ending parse"
             break
 
         if pre is not None:
@@ -40,15 +42,18 @@ def found_title(newtitle):
     global title, body
     if title and len(body):
         thebook.append(Section(title, body))
-        body = None
+        body = []
     title = newtitle
 
 def istitle(pre, cur, post):
     if not len(cur):
         return False
-    split = cur.split()
-    return all([split[0].isupper(), len(split) < 8,
-                not len(pre), not len(post)])
+    checks = ([cur.split()[0].isupper(),
+               #length before open paren
+               len(cur.split('(')[0].split()) < 8,
+               not len(pre), len(post.split()) <= 1])
+
+    return all(checks)
 
 
 
@@ -61,13 +66,39 @@ if __name__ == '__main__':
                 found_title(cur)
                 continue
 
-            if not body:
+            if not body and len(cur):
                 body = [cur]
-            else:
+            elif len(cur):
                 body.append(cur)
 
-    print len(thebook)
-    print "\n".join(map(str, thebook))
+        found_title(None)
+
+    recipes = set(imap(
+        str.lower,
+        (section.title for section in thebook))) - set(
+            ['index', 'dedicated', 'introduction'])
+
+    index = set(imap(
+        str.lower,
+        ifilter(len, thebook[-1].body))) - set(['page'])
+
+    if recipes != index:
+        print "***failure***\n"
+        print "differences: %s" % len(index.symmetric_difference(recipes))
+        print "index with no recipe found"
+        print index - recipes
+        print "recipes not found in the index"
+        print recipes - index
+        assert False, "recipes and index don't match."
+
+    for section in thebook:
+        print section.title
+        print "*" * len(section.title)
+        print ""
+        print '\n'.join(section.body)
+        print ""
+
+
 
 """
 
